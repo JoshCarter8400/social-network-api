7c0361a4147d7797d59366936b7669329a67478d
Josh Carter <joshcarter8400@gmail.com>
2020-12-07 13:37:12 -0500

    added reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,33 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
 const thoughtSchema = new Schema(
   {
@@ -17,7 +45,7 @@ const thoughtSchema = new Schema(
       type: String,
       required: true,
     },
-    reactions: [],
+    reactions: [reactionSchema],
   },
   {
     toJSON: {
